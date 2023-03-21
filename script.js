5b3467c8cefe7343598e1a949079f39484833422
Rybeardawg1 <rbarry3@students.stonehill.edu>
2023-03-21 00:58:08 -0400

    Some Updates

---
+++ script.js
@@ -0,0 +1,19 @@
+const subtitle = document.getElementsByClassName("card-subtitle")[0];
+
+const createWord = (text, index) => {
+  const word = document.createElement("span");
+  
+  word.innerHTML = `${text} `;
+  
+  word.classList.add("card-subtitle-word");
+  
+  word.style.transitionDelay = `${index * 40}ms`;
+  
+  return word;
+}
+
+const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
+
+const createSubtitle = text => text.split(" ").map(addWord);
+
+createSubtitle("Power! is a web application that allows you to send money to your friends and family. You can also add friends to your account and view your transaction history. ");
